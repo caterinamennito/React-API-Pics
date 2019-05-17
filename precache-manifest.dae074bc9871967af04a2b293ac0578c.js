acf883107146624e7c1f4c54637f063a18ef5888
caterinamennito <caterina.mennito@gmail.com>
2019-05-17 16:29:25 +0200

    build

---
+++ precache-manifest.dae074bc9871967af04a2b293ac0578c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0b0366da1804bb707298d101d2ef7d",
+    "url": "/React-API-Pics/index.html"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/React-API-Pics/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "9dbdb78b1a38260b71c4",
+    "url": "/React-API-Pics/static/js/main.4434eec6.chunk.js"
+  },
+  {
+    "revision": "50fe8f2884a16dcb2d42",
+    "url": "/React-API-Pics/static/js/runtime~main.8465f713.js"
+  }
+]);
